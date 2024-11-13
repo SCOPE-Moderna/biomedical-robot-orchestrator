8dad9a270dd9b232d3e72d224e0346341fafce78
Sam <iamtheyammer@gmail.com>
2024-11-13 11:12:34 -0500

    Add gRPC for JS, service

---
+++ nodes/nodes/grpc_ping/grpc-ping.ts
@@ -0,0 +1,26 @@
+import type { NodeAPI, Node, NodeMessage, NodeDef } from "node-red";
+import {NodeConnectorClient, PingResponse} from "../../service_pb2/service";
+import * as grpc from "@grpc/grpc-js";
+
+interface TestNodeDef extends NodeDef {}
+
+const service = new NodeConnectorClient(
+  "0.0.0.0:50051",
+  grpc.credentials.createInsecure(),
+  undefined,
+);
+
+module.exports = function (RED: NodeAPI) {
+  function GRPCPingNodeConstructor(this: Node, config: TestNodeDef): void {
+    RED.nodes.createNode(this, config);
+
+    this.on("input", async function (msg: NodeMessage, send, done) {
+      const payload = msg.payload.toString();
+      const response: PingResponse = await service.ping({ message: payload });
+      send([response.success, response.message]);
+      done();
+    });
+  }
+
+  RED.nodes.registerType("grpc-ping", GRPCPingNodeConstructor);
+};
