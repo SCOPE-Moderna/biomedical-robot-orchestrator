ff3bf509872ac0524e0804ee2e4a919dae2ff061
Sam <iamtheyammer@gmail.com>
2024-11-13 17:09:22 -0500

    Add xpeel-status node

---
+++ nodes/nodes/xpeel-status/xpeel-status.ts
@@ -0,0 +1,41 @@
+import type { NodeAPI, Node, NodeMessage, NodeDef } from "node-red";
+import {
+  NodeConnectorClient,
+  Empty,
+} from "../../node_connector_pb2/node_connector";
+import * as grpc from "@grpc/grpc-js";
+
+interface TestNodeDef extends NodeDef {}
+
+const service = new NodeConnectorClient(
+  "0.0.0.0:50051",
+  grpc.credentials.createInsecure(),
+  undefined,
+);
+
+module.exports = function (RED: NodeAPI) {
+  function XPeelStatusNodeConstructor(this: Node, config: TestNodeDef): void {
+    RED.nodes.createNode(this, config);
+
+    this.on("input", async function (msg: NodeMessage, send, done) {
+      const payload = (msg.payload as string | number).toString();
+      service.XPeelStatus(new Empty(), (error, response) => {
+        if (error) {
+          console.log(error);
+        }
+        send([
+          {
+            payload: [
+              response.error_code_1,
+              response.error_code_2,
+              response.error_code_3,
+            ],
+          },
+        ]);
+        done();
+      });
+    });
+  }
+
+  RED.nodes.registerType("xpeel-status", XPeelStatusNodeConstructor);
+};
