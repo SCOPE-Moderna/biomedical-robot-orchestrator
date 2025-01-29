b876f45f3f352eaa524331b16023e7af86a08ddd
AJ <ajecoder@gmail.com>
2025-01-29 10:25:04 -0500

    attempted installation of prettier + reformatting lower-case

--- nodes/nodes/lower-case/lower-case.ts
+++ nodes/nodes/lower-case/lower-case.ts
@@ -5,7 +5,7 @@ interface TestNodeDef extends NodeDef {}
 module.exports = function (RED: NodeAPI) {
   function LowerCaseNodeConstructor(this: Node, config: TestNodeDef): void {
     RED.nodes.createNode(this, config);
-    
+
     this.on("input", function (msg: NodeMessage, send, done) {
       msg.payload = (msg.payload as string).toLowerCase() + "!!!";
       send(msg);
