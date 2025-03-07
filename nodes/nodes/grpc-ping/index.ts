fb963e2e7e277c863b0ec0aa563326c0822f4c24
Sam <iamtheyammer@gmail.com>
2025-03-07 10:03:01 -0500

    Try using pluralize

--- nodes/nodes/grpc-ping/index.ts
+++ nodes/nodes/grpc-ping/index.ts
@@ -1,15 +1,15 @@
-import { BinaryConstants } from "google-protobuf";
+import { plural } from "pluralize";
 
 RED.nodes.registerType("grpc-ping", {
   category: "custom nodes",
   color: "#CA9258",
   defaults: {
-    name: { value: BinaryConstants.INVALID_FIELD_NUMBER.toString() },
+    name: { value: plural("node") },
   },
   inputs: 1,
   outputs: 1,
   icon: "file.svg",
   label: function () {
-    return this.name || "grpc-ping-default-label";
+    return this.name || plural("default");
   },
 });
