c29c3a6ca5f87fac159378431002a6779ed71065
scope-moderna-user <scope-moderna@olin.edu>
2025-04-02 14:16:15 -0400

    Convert tape remaining node

--- nodes/nodes/xpeel-taperemaining/xpeel-taperemaining.ts
+++ nodes/nodes/xpeel-taperemaining/xpeel-taperemaining.ts
@@ -1,8 +1,13 @@
 import type { NodeAPI, Node, NodeMessage, NodeDef } from "node-red";
 import { NodeConnectorClient } from "../../node_connector_pb2/node_connector";
 import * as grpc from "@grpc/grpc-js";
-import { XPeelGeneralRequest } from "../../node_connector_pb2/xpeel";
+import {
+  XPeelGeneralRequest,
+  XPeelStatusResponse,
+  XPeelTapeRemainingResponse,
+} from "../../node_connector_pb2/xpeel";
 import { RequestMetadata } from "../../node_connector_pb2/metadata";
+import { BaseNode, OrchestratorMessageInFlow } from "../nodeAPI";
 
 interface TestNodeDef extends NodeDef {}
 
@@ -51,3 +56,33 @@ module.exports = function (RED: NodeAPI) {
     XPeelTapeRemainingNodeConstructor,
   );
 };
+
+class XPeelTapeRemainingNode extends BaseNode {
+  async onInput(
+    msg: OrchestratorMessageInFlow,
+    requestMetadata: RequestMetadata,
+  ): Promise<NodeMessage> {
+    const response: XPeelTapeRemainingResponse = await new Promise(
+      (resolve, reject) => {
+        this.grpcClient.XPeelTapeRemaining(
+          new XPeelGeneralRequest({ metadata: requestMetadata }),
+          (error, response) => {
+            if (error) {
+              reject(error);
+            }
+            resolve(response);
+          },
+        );
+      },
+    );
+
+    msg.payload = {
+      deseals_remaining: response.deseals_remaining,
+      take_up_spool_space_remaining: response.take_up_spool_space_remaining,
+    };
+
+    return msg;
+  }
+}
+
+module.exports = XPeelTapeRemainingNode.exportable("xpeel-taperemaining");
